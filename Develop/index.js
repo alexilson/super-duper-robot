806cfe658bc6ba00e9ffbc7b36008e2325c233d7
Alex <ilson.alex@gmail.com>
2023-11-11 02:00:43 -0800

    Created array of inquiries

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+inq = require('inquirer')
+fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the title of your project:',
+        name: 'Title'
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'Username'
+    },
+    {
+        type: 'input',
+        message: 'Enter the description of your project:',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'Enter the installation instructions for your project:',
+        name: 'Installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information about your project:',
+        name: 'Usage'
+    },
+    {
+        type: 'list',
+        choices: ["MIT", "PIZZA", "MICHAELANGELO"],
+        message: 'Select a license for your project:',
+        name: 'License'
+    },
+    {
+        type: 'input',
+        message: 'Enter your contribution guidelines:',
+        name: 'Contributing'
+    },
+    {
+        type: 'input',
+        message: 'Enter your test instructions:',
+        name: 'Testing'
+    }
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +56,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inq.prompt(questions).then((response) => console.log(response))
