4a68f2d89554b0425b76d07c138cb64c7cbcb502
Alex <ilson.alex@gmail.com>
2023-11-20 18:56:56 -0800

    Adding parts for license badge generation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `https://img.shields.io/badge/${license}`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,6 +18,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license)
   return `# ${data.title}
 ### Created by: 
 https://www.github.com/${data.username}
@@ -26,7 +33,8 @@ ${data.installation}
 ${data.usage}
 
 ## License Information
-${data.license}
+${licenseBadge} - 
+
 
 ## How To Contribute
 ${data.contributing}
